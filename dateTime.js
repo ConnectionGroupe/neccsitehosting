a1c83f1d75d4b998678ea2eaad7330ff88b6220f
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2023-10-01 22:49:22 -0500

    Create dateTime.js

---
+++ dateTime.js
@@ -0,0 +1,23 @@
+function formatEpochTime(epochTime) {
+    const date = new Date(epochTime * 1000);
+    const offset = (date.getTimezoneOffset() / 60);
+    const hours = ((date.getHours()+5-offset).toString().padStart(2, '0')) ;
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    if (hours > 12) {
+        return `${hours % 12 === 0 ? 12 : hours % 12}:${minutes} PM`;
+    }
+    else {
+        return `${hours % 12 === 0 ? 11 : hours % 12}:${minutes} AM`;
+    }
+}
+function updateClassDate() {
+    const classDateElements = document.querySelectorAll('.dateTime');
+
+    classDateElements.forEach((element) => {
+        const epochTime = parseInt(element.textContent);
+        if (!isNaN(epochTime)) {
+            element.textContent = formatEpochTime(epochTime);
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', updateClassDate);
