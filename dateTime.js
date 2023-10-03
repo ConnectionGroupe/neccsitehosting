83c60709cad53126d4d91a032c70945538334614
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2023-10-02 20:54:29 -0500

    more tests

--- dateTime.js
+++ dateTime.js
@@ -1,24 +1,24 @@
 function formatEpochTime(epochTime) {
-    const date = new Date(epochTime * 1000);
-    const offset = (date.getTimezoneOffset() / 60);
-    const hours = ((date.getHours()+5-offset).toString().padStart(2, '0')) ;
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    if (hours > 12) {
-        return `${hours % 12 === 0 ? 12 : hours % 12}:${minutes} PM`;
-    }
-    else {
-        return `${hours % 12 === 0 ? 11 : hours % 12}:${minutes} AM`;
-    }
+	const date = new Date(epochTime * 1000);
+	const offset = (date.getTimezoneOffset() / 60);
+	const hours = ((date.getHours()+5-offset).toString().padStart(2, '0')) ;
+	const minutes = date.getMinutes().toString().padStart(2, '0');
+	if (hours > 12) {
+		return `${hours % 12 === 0 ? 12 : hours % 12}:${minutes} PM`;
+	}
+	else {
+		return `${hours % 12 === 0 ? 11 : hours % 12}:${minutes} AM`;
+	}
 }
 function updateClassDate() {
-    const classDateElements = document.querySelectorAll('.dateTime');
+	const classDateElements = document.querySelectorAll('.dateTime');
 
-    classDateElements.forEach((element) => {
-        const epochTime = parseInt(element.textContent);
-        if (!isNaN(epochTime)) {
-            element.textContent = formatEpochTime(epochTime);
-        }
-    });
+	classDateElements.forEach((element) => {
+		const epochTime = parseInt(element.textContent);
+		if (!isNaN(epochTime)) {
+			element.textContent = formatEpochTime(epochTime);
+		}
+	});
 }
 document.addEventListener('DOMContentLoaded', updateClassDate);
 
@@ -30,9 +30,9 @@ var navbar = document.getElementById("topSchedule");
 var sticky = navbar.offsetTop;
 
 function stickFunc() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky");
+	}
 }
