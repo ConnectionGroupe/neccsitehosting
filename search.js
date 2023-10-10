da259b296b30a6b37ea731c5b95c9bfa05aa3f9d
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2023-10-10 18:16:31 -0500

    Create search.js

---
+++ search.js
@@ -0,0 +1,16 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+function submitted(event) {
+    const x = document.querySelectorAll('.containerSchedule');
+    x.forEach((element) => {
+        const name = (element.id);
+        if (!name.toLowerCase().includes(q.value.toLowerCase())) {
+            element.setAttribute('style', 'display:none;')
+        } else {
+            element.setAttribute('style', 'display:content;')
+        }
+    });
+
+}
+
+f.addEventListener('input',submitted);
