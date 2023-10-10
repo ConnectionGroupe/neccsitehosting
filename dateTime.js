7c0bbf7f58ca4cdf3c517704804d9745dfa5bf07
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2023-10-09 20:32:26 -0500

    Update dateTime.js

--- dateTime.js
+++ dateTime.js
@@ -1,15 +1,18 @@
 function formatEpochTime(epochTime) {
-	const date = new Date(epochTime * 1000);
+	let date = new Date(epochTime * 1000);
 	const offset = (date.getTimezoneOffset() / 60);
-	const hours = ((date.getHours()+5-offset).toString().padStart(2, '0')) ;
-	const minutes = date.getMinutes().toString().padStart(2, '0');
-	if (hours > 12) {
-		return `${hours % 12 === 0 ? 12 : hours % 12}:${minutes} PM`;
-	}
-	else {
-		return `${hours % 12 === 0 ? 11 : hours % 12}:${minutes} AM`;
+	date.setHours(date.getHours()+5-offset)
+	return date.toLocaleString([],{hour: "2-digit", minute:'2-digit'})
+}
+
+function fromatDateMonthTime(epochTime) {
+	let date = new Date(epochTime * 1000);
+	const offset = (date.getTimezoneOffset() / 60);
+	date.setHours(date.getHours()+5-offset)
+	return (date.toLocaleString([],{weekday: "long", month:'long', day:'2-digit'}))
 	}
 }
+
 function updateClassDate() {
 	const classDateElements = document.querySelectorAll('.dateTime');
 
@@ -19,20 +22,14 @@ function updateClassDate() {
 			element.textContent = formatEpochTime(epochTime);
 		}
 	});
+
+	const classDateYYElements = document.querySelectorAll('.date');
+
+	classDateYYElements.forEach((element) => {
+		const epochTime = parseInt(element.textContent);
+		if (!isNaN(epochTime)) {
+			element.textContent = fromatDateMonthTime(epochTime);
+		}
+	});
 }
 document.addEventListener('DOMContentLoaded', updateClassDate);
-
-
-window.onscroll = function() {stickFunc()};
-
-var navbar = document.getElementById("topSchedule");
-
-var sticky = navbar.offsetTop;
-
-function stickFunc() {
-	if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky")
-	} else {
-		navbar.classList.remove("sticky");
-	}
-}
