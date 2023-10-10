c7b0083ccf06fddc3c13eab34836babd902e2841
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2023-10-10 18:29:51 -0500

    Update search.js

--- search.js
+++ search.js
@@ -4,10 +4,10 @@ function submitted(event) {
     const x = document.querySelectorAll('.containerSchedule');
     x.forEach((element) => {
         const name = (element.id);
-        if (!name.toLowerCase().includes(q.value.toLowerCase())) {
-            element.setAttribute('style', 'display:none;')
-        } else {
+        if (name.toLowerCase().includes(q.value.toLowerCase())) {
             element.setAttribute('style', 'display:content;')
+        } else {
+            element.setAttribute('style', 'display:none;')
         }
     });
 
