e3e90786c7a0baceb1c3df57934491e4508b88fa
ConnectionGroupe <147683957+ConnectionGroupe@users.noreply.github.com>
2023-10-12 00:15:08 -0500

    Create standings.js

---
+++ standings.js
@@ -0,0 +1,50 @@
+function update(){
+    const game = document.getElementById('games');
+    const div = document.getElementById('divisions');
+    const regions = document.getElementById('regions');
+    const x = (game.value+div.value+regions.value);
+    console.log(x)
+    document.querySelectorAll('.leaderboard').forEach((element) => {
+        if(element.className.replace('leaderboard ','') === x){
+            element.style = 'display: content;'
+        }
+        else{
+            element.style = 'display: none;'
+        }
+    });
+
+    document.querySelectorAll(".dp").forEach((element) => {
+        if(element.className === game.value+' dp'){
+            element.style = 'display: content;'
+
+        }
+        else{
+
+            element.style = 'display: none;'
+        }
+    });
+
+    document.querySelectorAll(".dp").forEach((element) => {
+        if(element.className === game.value+div.value+' dp'){
+            element.style = 'display: content;'
+
+        }
+        else{
+            element.style = 'display: none;'
+        }
+    });
+}
+
+function add(){
+    document.getElementById('games').addEventListener('input',fix);
+    document.getElementById('divisions').addEventListener('input',update);
+    document.getElementById('regions').addEventListener('input',update);
+}
+
+function fix(){
+    document.getElementById('divisions').selectedIndex=0
+    document.getElementById('regions').selectedIndex=0
+    update()
+}
+
+document.addEventListener('DOMContentLoaded', add);
